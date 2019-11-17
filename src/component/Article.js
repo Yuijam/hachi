7f57495a4259c6c878c93c2beeddd376d1cf2470
yuijam <yuijam@live.com>
2019-11-17 10:40:14 +0900

    reset css; update article

--- src/component/Article.js
+++ src/component/Article.js
@@ -1,39 +1,37 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './css/Article.css'
 import ReactMarkdown from 'react-markdown'
 import CodeBlock from './CodeBlock'
+import { Typography } from 'antd'
+const { Title, Paragraph } = Typography;
 
-class Article extends Component{
+class Article extends Component {
 
-    render(){
-        const {articleData, isOwner, onDelete, toEditUrl} = this.props
-        const {title, text_origin, writeTime} = articleData
-        const writeTimeStr = new Date(writeTime).toLocaleTimeString()
-        return (
-            <div className='article-body'>
-                <div>
-                    <h4>{title}</h4>
-                </div>
-                <div className="text_md">
-                    <ReactMarkdown source={text_origin} renderers={{code: CodeBlock}}/>
-                </div>
+  render() {
+    const { articleData, isOwner, onDelete, toEditUrl } = this.props
+    const { title, text_origin, writeTime } = articleData
+    const writeTimeStr = new Date(writeTime).toLocaleTimeString()
+    return (
+      <Paragraph className='article-body'>
+        <Title level={2}>{title}</Title>
+        <Paragraph>
+          <ReactMarkdown source={text_origin} renderers={{ code: CodeBlock }} />
+        </Paragraph>
+        <p className='write-time'>{`created at ${writeTimeStr}`}</p>
 
-                <p className='write-time'>{`created at ${writeTimeStr}`}</p>
+        {isOwner ?
+          <div className='opt'>
+            <Link onClick={onDelete} to='#'>Delete</Link>
 
-                {isOwner ? 
-                    <div className='opt'>
-                        <Link onClick={onDelete} to='#'>Delete</Link>
-                        
-                        <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
 
-                        <Link replace={true} to={{pathname:toEditUrl, articleData:articleData}}>Edit</Link>
-                    </div>
-                    : ''}
-                
-            </div>
-        )
-    }
+            <Link replace={true} to={{ pathname: toEditUrl, articleData: articleData }}>Edit</Link>
+          </div>
+          : ''}
+      </Paragraph>
+    )
+  }
 }
 
 export default Article
